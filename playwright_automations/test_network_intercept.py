c41377d7f3602c50d80146f0567116ad6f3d8b0c
Supriya Mainalli <supriyamainallio@gmail.com>
2026-06-13 20:44:14 +0530

    adds network intercept for request mocking

--- playwright_automations/test_network_intercept.py
+++ playwright_automations/test_network_intercept.py
@@ -1,13 +1,15 @@
 from playwright.sync_api import Playwright, expect
 import time
 
+
+# intercept response -> fulfill
 data = {"data":[],"message":"No Orders"}
-def network_intercept(route):
+def network_intercept_response(route):
     route.fulfill(
         json = data
     )
 
-def test_network1(playwright: Playwright):
+def test_network_response_mock(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
@@ -15,7 +17,7 @@ def test_network1(playwright: Playwright):
     page.goto("https://rahulshettyacademy.com/client")
 
     # Intercept the network and mock the response
-    page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*", network_intercept)
+    page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*", network_intercept_response)
     page.get_by_placeholder("email@example.com").fill("supriyam@yopmail.com")
     page.get_by_placeholder("enter your passsword").fill("Iamking@000")
     page.locator("#login").click()
@@ -24,3 +26,25 @@ def test_network1(playwright: Playwright):
     text = page.locator(".mt-4").text_content().strip()
     print(text)
     assert text == "You have No Orders to show at this time. Please Visit Back Us"
+
+
+# intercept request
+def network_intercept_request(route):
+    route.continue_(url="https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=6a2d70c417ee3e78bada0ce2")
+
+def test_network_request_mock(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto("https://rahulshettyacademy.com/client")
+    # Intercept the network and mock the response
+    page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=*", network_intercept_request)
+    page.get_by_placeholder("email@example.com").fill("supriyam@yopmail.com")
+    page.get_by_placeholder("enter your passsword").fill("Iamking@000")
+    page.locator("#login").click()
+    page.get_by_role("button", name="ORDERS").click()
+    time.sleep(2)
+    page.get_by_role("button", name="View").first.click()
+    text = page.locator(".blink_me").text_content().strip()
+    assert text == "You are not authorize to view this order"
